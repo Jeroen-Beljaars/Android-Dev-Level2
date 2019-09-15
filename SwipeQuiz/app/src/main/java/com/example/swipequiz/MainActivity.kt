481384bc6ef2ffb14d4c107f4f78d1fff60adebc
J.C. Beljaars <jeroen.beljaars@hva.nl>
2019-09-15 17:32:42 +0200

    Finished level 2

---
+++ SwipeQuiz/app/src/main/java/com/example/swipequiz/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.swipequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews(){
+        // Initialize the recycler view with a linear layout manager, adapter
+        rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        rvQuestions.adapter = questionAdapter
+        rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        createItemTouchHelper().attachToRecyclerView(rvQuestions)
+
+        questions.add(Question("Lorem ipsum", true))
+        questions.add(Question("Dolor amet", false))
+        questions.add(Question("Test lorem", true))
+        questions.add(Question("A == A", true))
+        questionAdapter.notifyDataSetChanged()
+    }
+
+    /**
+     * Create a touch helper to recognize when a user swipes an item from a recycler view.
+     * An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+     * and uses callbacks to signal when a user is performing these actions.
+     */
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val currentQuestion = questions[position]
+                if (currentQuestion.answer == (direction == ItemTouchHelper.RIGHT)){
+                    Snackbar.make(tvInstruction, "Your answer is correct!", Snackbar.LENGTH_SHORT).show()
+                } else{
+                    Snackbar.make(tvInstruction, "Your answer is incorrect!", Snackbar.LENGTH_SHORT).show()
+                }
+
+                questionAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+}
---
+++ SwipeQuiz/app/src/main/java/com/example/swipequiz/Question.kt
@@ -0,0 +1,6 @@
+package com.example.swipequiz
+
+data class Question(
+    var question: String,
+    var answer: Boolean
+)
